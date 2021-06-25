92fe9feb9cd393fac3decb71a802430999b1400c
wu-hongkai <1901210530@pku.edu.cn>
2021-06-26 01:36:27 +0800

    修改Autowired注解的处理

--- src/main/java/com.wu/core/BeanFactory.java
+++ src/main/java/com.wu/core/BeanFactory.java
@@ -16,56 +16,56 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /*
-* BeanFactory负责的是IOC的核心功能：
-*   - 将扫描到的类中使用指定注解的都做初始化，并将实例保存到IOC容器中
-* */
+ * BeanFactory负责的是IOC的核心功能：
+ *   - 将扫描到的类中使用指定注解的都做初始化，并将实例保存到IOC容器中
+ * */
 public class BeanFactory {
     //IOC容器：key/value=指定类的Class对象/指定类的实例
     private static Map<Class<?>, Object> ioc = new ConcurrentHashMap<>();
     /**
-     * set存放带有 @AutoWired 注解修饰的属性的类（将set转成线程安全的set）
+     * set存放带有 @AutoWired 注解修饰的属性的类（将set转成线程安全的set）：避免重复遍历整个classList
      */
     private static Set<Class<?>> beansHasAutoWiredField = Collections.synchronizedSet(new HashSet<>());
 
     /*
-    * getBean：通过类的Class对象获取对应的实例
-    * */
-    public static Object getBean(Class<?> clazz){
+     * getBean：通过类的Class对象获取对应的实例
+     * */
+    public static Object getBean(Class<?> clazz) {
         return ioc.get(clazz);
     }
 
     /**
-     * 根据类列表 classList 来查找所有需要初始化的类并放入 Component 工厂，
-     * 并且处理类中所有带 @AutoWired 注解的属性的依赖问题。
+     * 1、遍历classList中的所有类，创建实例加入IOC容器
+     * 2、处理AOP注解的bean对象，更新IOC容器中的被代理对象
+     * 3、遍历set存放的所有标注了@AutoWired的Class对象，做注入处理
      */
     public static void initBean(List<Class<?>> classList) throws Exception {
         // 因为"类定义"在后续处理 类中的 @RequestMapping 注解，生成处理器时还要使用，
         // 因此这里要创建新容器，不能修改原引用
         List<Class<?>> classesToCreate = new ArrayList<>(classList);
-        // 保存被 @Aspect 注解的切面类
+        // 保存被 @Aspect 注解的切面类：避免重复遍历整个classList
         List<Class<?>> aspectClasses = new ArrayList<>();
 
-        //1、遍历扫描到的所有类
-        //2、对标注了@Autowired的属性做依赖注入（createBean()中）
+        //1、遍历扫描到的所有类，都创建实例加入IOC容器
         for (Class<?> clazz : classesToCreate) {
-            if (clazz.isAnnotationPresent(MyAspect.class)) {
+            createBean(clazz);
+            if(clazz.isAnnotationPresent(MyAspect.class))
                 aspectClasses.add(clazz);
-            } else {
-                createBean(clazz);
-            }
         }
-        // 3、使用动态代理处理AOP
+
+        // 2、处理AOP注解的bean对象，更新IOC容器中的被代理对象
         resolveAOP(aspectClasses);
 
-        // 4、标注@Autowired的属性在上面的createBean已经注入过了，但在引入aop后可能有的属性发生了更新，需要重新注入
+        // 3、遍历set存放的所有标注了@AutoWired的Class对象，做注入处理
         for (Class<?> clazz : beansHasAutoWiredField) {
-            createBean(clazz);
+            resolveAutowired(clazz);
         }
     }
 
+
     /**
-     * 通过 Class 对象创建实例
-     *
+     * 通过 Class 对象创建实例，加入IOC容器
+     *  - 将属性标注了@Autowired的Class对象加入set：beansHasAutoWiredField
      * @param clazz 需要创建实例的 Class 对象
      */
     private static void createBean(Class<?> clazz) throws IllegalAccessException, InstantiationException {
@@ -75,39 +75,23 @@ public class BeanFactory {
                 && !clazz.isAnnotationPresent(MyService.class)) {
             return;
         }
-
-        Object bean = null;
-        // 初始化对象
-        bean = clazz.newInstance();
-
-        // 遍历类中所有定义的属性，如果属性带有 @AutoWired 注解，则需要注入对应依赖
-        for (Field field : bean.getClass().getDeclaredFields()) {
-            if (!field.isAnnotationPresent(MyAutoWired.class)) {
-                continue;
+        //将所有属性标注了@Autowired的Class对象加入set
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(MyAutoWired.class)) {
+                beansHasAutoWiredField.add(clazz);
             }
-            // 将需要注入其他 Bean 的类保存起来，因为等 AOP 代理类生成之后，需要更新它们
-            BeanFactory.beansHasAutoWiredField.add(clazz);
-            Class<?> fieldType = field.getType();
-            field.setAccessible(true);
-            if (fieldType.isInterface()) {
-                // 如果依赖的类型是接口，则查询其实现类,
-                // class1.isAssignableFrom(class2) = true 代表class2是class1类型，可分配class2对象给class1
-                for (Class<?> key : ioc.keySet()) {
-                    if (fieldType.isAssignableFrom(key)) {
-                        fieldType = key;
-                        break;
-                    }
-                }
-            }
-            field.set(bean, BeanFactory.getBean(fieldType));
         }
-        // todo 这里可能AutoWired注入失败，例如存在循环依赖，或者bean工厂中根本不存在，目前暂时先不处理
+        // 初始化对象
+        Object bean = clazz.newInstance();
+        //加入IOC容器
         ioc.put(clazz, bean);
+
     }
 
     /**
-     * 对于所有被 @Aspect 注解修饰的类，
+     * 对于所有被 @Aspect 注解修饰的类：
      * 遍历他们定义的方法，处理 @Pointcut、@Before 以及 @After 注解
+     * 创建被切入对象的代理对象，更新到IOC容器中
      */
     private static void resolveAOP(List<Class<?>> aspectClasses)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
@@ -138,8 +122,6 @@ public class BeanFactory {
                     pointcutName = m.getName();
                 }
             }
-
-
             for (Method m : bean.getClass().getDeclaredMethods()) {
                 //处理@MyBefore：该注解用在方法上，希望在切点对象之前执行
                 //注解中指定的value是切入点的唯一标识pointcutName，这样就能实现前置方法和切入点的绑定
@@ -160,7 +142,6 @@ public class BeanFactory {
                     }
                 }
             }
-
             // 获取代理对象：这里用到了动态代理
             Object proxy = new AOPProxy().createProxy(bean, before, after,
                     target, methodName.substring(0, methodName.indexOf("(")));
@@ -169,4 +150,36 @@ public class BeanFactory {
         }
     }
 
+    /*
+     * 依赖注入，处理一个bean对象中的所有@Autowired属性，完成注入
+     * */
+    private static void resolveAutowired(Class<?> clazz) {
+        //从IOC容器中获取clazz对应的bean
+        Object bean = ioc.get(clazz);
+
+        // 遍历类中所有定义的属性，如果属性带有 @AutoWired 注解，则需要注入对应依赖
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            if (!field.isAnnotationPresent(MyAutoWired.class)) {
+                continue;
+            }
+            Class<?> fieldType = field.getType();
+            field.setAccessible(true);
+            if (fieldType.isInterface()) {
+                // 如果依赖的类型是接口，则查询其实现类,
+                // class1.isAssignableFrom(class2) = true 代表class2是class1类型，可分配class2对象给class1
+                for (Class<?> key : ioc.keySet()) {
+                    if (fieldType.isAssignableFrom(key)) {
+                        fieldType = key;
+                        break;
+                    }
+                }
+            }
+            try {
+                //依赖注入的关键，提取IOC容器里的最新的bean做注入
+                field.set(bean, BeanFactory.getBean(fieldType));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
